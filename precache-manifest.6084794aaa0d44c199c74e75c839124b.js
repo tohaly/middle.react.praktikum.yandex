d400825d897d3715602f2fa35b8ec1eab83c64f0
Anton Voitenko <voytenko.ad@yandex.ru>
2020-04-27 03:21:50 +0800

    Updates

---
+++ precache-manifest.6084794aaa0d44c199c74e75c839124b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40684bb9f50122ebb469255d6cf2db6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "58e07f978c3fd53216b2",
+    "url": "./static/css/main.e17955e6.chunk.css"
+  },
+  {
+    "revision": "f6b9222d055dd5d97b5f",
+    "url": "./static/js/2.87746a6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.87746a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e07f978c3fd53216b2",
+    "url": "./static/js/main.3beedf78.chunk.js"
+  },
+  {
+    "revision": "cb716a7f8d2a5423406b",
+    "url": "./static/js/runtime-main.892f4492.js"
+  }
+]);
